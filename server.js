db68a3d81baf71c6dc857c7b9f8cc4203f572b7c
michaelmoreno.dev@gmail.com <michaelmoreno.dev@gmail.com>
2020-11-05 16:32:52 -0600

    Public folder setup

--- server.js
+++ server.js
@@ -2,6 +2,8 @@ const express = require('express');
 const app = express();
 const PORT = 3000;
 
+app.use('/public', express.static('public'))
+
 app.get('/', (req, res) => {
   res.render('index.ejs')
 })
