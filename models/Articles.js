6bc163e1e92dd9584e04a4c4521e11a929c4a6ed
michaelmoreno.dev@gmail.com <michaelmoreno.dev@gmail.com>
2020-11-05 17:02:36 -0600

    _display partial setup

---
+++ models/Articles.js
@@ -0,0 +1,16 @@
+const mongoose = require('mongoose');
+
+const articleSchema = new mongoose.Schema({
+  title: {
+    type: String,
+    required: true,
+  },
+  content: {
+    type: String,
+    required: true,
+  },
+  date: {
+    type: Date,
+    default: Date.now(),
+  }
+})
--- server.js
+++ server.js
@@ -5,7 +5,13 @@ const PORT = 3000;
 app.use('/public', express.static('public'))
 
 app.get('/', (req, res) => {
-  res.render('index.ejs')
+  res.render('index.ejs', { articles: [
+    {
+      title: 'test',
+      content: 'test',
+      date: Date.now(),
+    }
+  ]})
 })
 
 app.listen(PORT, console.log('Listening on', PORT))
