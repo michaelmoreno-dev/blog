ff729f51960bd8a18a68cd609af0e516825d2718
michaelmoreno.dev@gmail.com <michaelmoreno.dev@gmail.com>
2020-11-05 16:21:12 -0600

    Added header partial

--- server.js
+++ server.js
@@ -2,4 +2,8 @@ const express = require('express');
 const app = express();
 const PORT = 3000;
 
+app.get('/', (req, res) => {
+  res.render('index.ejs')
+})
+
 app.listen(PORT, console.log('Listening on', PORT))
